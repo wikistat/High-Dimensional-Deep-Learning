e5c4299972cfb7dcf19a3726b1e48a69fc4008de
Brendan Guillouet <brendan.guillouet@gmail.com>
2018-05-23 18:09:28 +0200

    Illustration function ok

--- HumanActivityRecognition/utils/illustration.py
+++ HumanActivityRecognition/utils/illustration.py
@@ -1,9 +1,49 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import itertools
+import random
 
-## ACP##
+#############
+## SIGNAUX ##
+#############
 
-def plot_variance_acp(fig, ax, acp, X_acp): 
+def plot_signaux(fig, x, y, signals, color_dic, nb_sample_per_activity, 
+                 linestyle_per_activity, linewidth_per_activity, shuffle=False):
+    index = []
+    for k,v in itertools.groupby(sorted(enumerate(y), key=lambda x : x[1]), key=lambda x : x[1]):
+        lv = np.array(list(v))
+        index.extend(lv[:nb_sample_per_activity[k],0].astype(int))
+    index = sorted(index)  
+    
+    x_range = range(x.shape[1])
+
+    
+    data_plot = list(zip(x[index], y[index]))
+    if shuffle:
+        random.shuffle(data_plot)
+    for isignal, signal in enumerate(signals):
+        labels=[]
+        ax = fig.add_subplot(3,2,isignal+1)
+        for values,label in data_plot:  
+            color = color_dic[label]
+            lw = linewidth_per_activity[label]
+            ls = linestyle_per_activity[label]
+            if label in labels:
+                ax.plot(x_range,values[:,isignal], color=color, linewidth=lw, linestyle=ls)
+            else:
+                ax.plot(x_range,values[:,isignal], color=color, linewidth=lw, linestyle=ls, label = label)
+                labels.append(label)
+        plt.legend(fontsize=15,ncol=2)
+        ax.set_title(signal, fontsize=25)
+    plt.tight_layout()
+
+
+#########
+## ACP ##
+#########
+
+def plot_variance_acp(fig, acp, X_acp): 
+    ax = fig.add_subplot(1,2,1)
     ax.bar(range(10), acp.explained_variance_ratio_[:10]*100, align='center',
         color='grey', ecolor='black')
     ax.set_xticks(range(10))
@@ -14,31 +54,30 @@ def plot_variance_acp(fig, ax, acp, X_acp):
     ax = fig.add_subplot(1,2,2)
     box=ax.boxplot(X_acp[:,0:10])
     ax.set_title("Distribution des premieres composantes", fontsize=20)
+
+def plot_pca(ax, X, acp, nbc, nbc2, colors, markersizes):
+    ax.scatter(X[:,nbc-1],X[:,nbc2-1],marker=".", color= colors, s=markersizes)
+    ax.set_xlabel("PC%d : %.2f %%" %(nbc,acp.explained_variance_ratio_[nbc-1]*100), fontsize=15)
+    ax.set_ylabel("PC%d : %.2f %%" %(nbc2,acp.explained_variance_ratio_[nbc2-1]*100), fontsize=15)
+
     
-    
-def plot_projection_acp(fig, ax, acp, X_acp, colors=None, labels=None, markersizes=None):
-    
+def plot_projection_acp(fig, X_acp, acp, colors=None, markersizes=None, color_dic = None):
+    ax = fig.add_subplot(1,2,1)
     N = X_acp.shape[0]
     if colors==None:
         colors = ["black"]*N
-    if labels==None:
-        labels = [""]*N
     if markersizes==None:
         markersizes = [1]*N
     
-    for x1, x2, c, l, m in zip(X_acp[:,0], X_acp[:,1], colors, labels, markersizes):
-        if l=="":
-            ax.plot(x1,x2,marker=".", color= c, markersize = m)
-        else:
-            ax.plot(x1,x2,marker=".", color=c, label=l, markersize = m, linestyle = None)
-   
+    plot_pca(ax, X_acp, acp, 1, 2, colors, markersizes)
     ax.set_title("Projection des invididus sur les \n  deux premieres composantes", fontsize=20)
     ax.set_xlim(X_acp[:,0].min()-5,X_acp[:,0].max()+5)
     ax.set_ylim(X_acp[:,1].min()-1,X_acp[:,1].max()+1)
-    ax.set_xlabel("PC1 : %.2f %%" %(acp.explained_variance_ratio_[0]*100), fontsize=15)
-    ax.set_ylabel("PC2 : %.2f %%" %(acp.explained_variance_ratio_[1]*100), fontsize=15)
-
-    plt.legend(fontsize=15, ncol=2)
+    
+    if not(color_dic is None):
+        from matplotlib.lines import Line2D
+        legend_elements = [Line2D([0], [0], color=color,marker=".", linestyle=None, markersize=15, label=act)for act, color in color_dic.items()]
+        plt.legend(handles=legend_elements, fontsize=12, ncol=2)
 
 
     ax = fig.add_subplot(1,2,2)
@@ -46,11 +85,12 @@ def plot_projection_acp(fig, ax, acp, X_acp, colors=None, labels=None, markersiz
         ax.text(x1,x2,str(f), horizontalalignment="center", verticalalignment="center", color="red")
     ax.set_xlim(acp.components_[0].min()-0.002,acp.components_[0].max()+0.002)
     ax.set_ylim(acp.components_[1].min()-0.01,acp.components_[1].max()+0.01)
-    ax.set_title("Projection des features sur les \n  deux premisres composantes", fontsize=20)
+    ax.set_title("Projection des features sur les \n  deux premieres composantes", fontsize=20)
     
     
-
-#Anomaly Detection#
+#####################
+# Anomaly Detection #
+#####################
 
 def plot_decision_function(fig, ax, method, X, y_pred, s=40, method_name="SVM", colors=None, labels=None, markersizes=None):
     N = X.shape[0] 
